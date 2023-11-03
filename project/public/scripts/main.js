a409d73ca3408d52086682e8b39b4be7cebe33af
patwarv <90049805+patwarv@users.noreply.github.com>
2023-11-03 06:13:12 -0400

    Lot of changes, search works in its raw form

--- project/public/scripts/main.js
+++ project/public/scripts/main.js
@@ -1,36 +1,59 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const fetchDataButton = document.getElementById('fetchDataButton');
-    const dataIndexInput = document.getElementById('dataIndexInput');
+    const keywordInput = document.getElementById('dataIndexInput'); // assuming this is your search bar input
     const dataTableHead = document.querySelector('#dataTable thead tr');
-    const dataTableBody = document.querySelector("#dataTable tbody tr");
-
-    
+    const dataTableBody = document.querySelector("#dataTable tbody");
 
     fetchDataButton.addEventListener('click', function() {
-        const dataIndex = dataIndexInput.value;
-        if(dataIndex){
-            fetch(`https://in211.scanurag.com/realResources.json`)
-                .then(response => response.json())
-                .then(data => {
-                    dataTableHead.innerHTML = '';
-                    dataTableBody.innerHTML = '';
-                    for(const key in data){
-                        dataTableHead.insertAdjacentHTML('beforeend', `<th>${key}</th>`);
-                        dataTableBody.insertAdjacentHTML('beforeend', `<td>${data[key]}</td>`);
-                    }
-                    $('#dataIndexModal').modal('hide');
-                })
-                .catch(error => {
-                    console.error("Error fetching data: ", error);
-                    dataTableBody.innerHTML = `<td colspan="2">Error fetching data. Please try again.</td>`;
-                })
-                .finally(() => {
-                    $("#dataIndexModal").modal('hide');
-                    $("body").removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                })
-                
-        }
-    })
+        const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
+        fetch(`/services`)
+            .then(response => response.json())
+            .then(data => {
+                dataTableHead.innerHTML = ''; // Clear existing data
+                dataTableBody.innerHTML = '';
 
-})
+                let resultsFound = false;
+                
+                data.forEach(entry => {
+                    // Check if any of the object's values contain the keyword
+                    for (let [key, value] of Object.entries(entry)) {
+                        if (String(value).toLowerCase().includes(keyword)) {
+                            resultsFound = true;
+                            
+                            // Build table header (only once)
+                            if (dataTableHead.children.length === 0) {
+                                Object.keys(entry).forEach(k => {
+                                    dataTableHead.insertAdjacentHTML('beforeend', `<th>${k}</th>`);
+                                });
+                            }
+
+                            // Build table row for this entry
+                            let row = '<tr>';
+                            Object.values(entry).forEach(val => {
+                                row += `<td>${val}</td>`;
+                            });
+                            row += '</tr>';
+                            dataTableBody.insertAdjacentHTML('beforeend', row);
+                            break;
+                        }
+                    }
+                });
+
+                if (!resultsFound) {
+                    dataTableBody.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
+                }
+
+                $('#dataIndexModal').modal('hide');
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                dataTableBody.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
+            })
+            .finally(() => {
+                // Code to close modal and clean up, if necessary
+                $("#dataIndexModal").modal('hide');
+                $("body").removeClass('modal-open');
+                $('.modal-backdrop').remove();
+            });
+    });
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'project', 'public'))); // Use 'project' folder for static files
+
+// Set up the proxy to forward requests from /api to your target server
+app.use('/api', createProxyMiddleware({
+  target: 'https://in211.scanurag.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '',
+  },
+}));
+
+const PORT = 3033;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Endpoint to serve local mock.json
+app.get('/services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'public', 'data', 'mock.json'));
+});
+
+// Default route to serve main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project', 'public', 'main_page.html')); // Path to main_page.html
+});
+
