5a05fe0b790200c0fa6bca53364a07537250250f
rhit-chungea <chungea@rose-hulman.edu>
2023-11-06 00:11:13 -0500

    can now search by food and housing by using buttons

--- project/public/scripts/main.js
+++ project/public/scripts/main.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const dataTableBody = document.querySelector("#dataTable tbody");
     const search = "county";
     const cardContainer = document.getElementById('cardContainer');
+    const foodButton = document.getElementById('foodButton');
+    const housingButton = document.getElementById('housingButton');
 
     fetchDataButton.addEventListener('click', function() {
         const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
@@ -12,32 +14,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         fetch(`/services`)
             .then(response => response.json())
             .then(data => {
-                dataTableHead.innerHTML = ''; // Clear existing data
-                dataTableBody.innerHTML = '';
-                cardContainer.innerHTML = '';
+                cardContainer.innerHTML = ''; // Clear existing data
                 let resultsFound = false;
                 
                 data.forEach(entry => {
-                    // Check if any of the object's values contain the keyword
-                    // for (let [key, value] of Object.entries(entry)) {
+
                         if (entry[search].toLowerCase().includes(keyword)) {
                             resultsFound = true;
-                            
-                            // // Build table header (only once)
-                            // if (dataTableHead.children.length === 0) {
-                            //     Object.keys(entry).forEach(k => {
-                            //         dataTableHead.insertAdjacentHTML('beforeend', `<th>${k}</th>`);
-                            //     });
-                            // }
-
-                            // Build table row for this entry
-                            // let row = '<tr>';
-                            // Object.values(entry).forEach(val => {
-                            //     row += `<td>${val}</td>`;
-                            // });
-                            // row += '</tr>';
-                            // dataTableBody.insertAdjacentHTML('beforeend', row);
-                           
 
                             
                             const card = document.createElement('div');
@@ -103,7 +86,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 );
 
                 if (!resultsFound) {
-                    dataTableBody.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
+                    cardContainer.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
                 }
 
                 $('#dataIndexModal').modal('hide');
@@ -113,10 +96,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 showthis.style.display = 'block';
                 document.getElementById('bigName').innerHTML = keywordInput.value.toUpperCase() + ' COUNTY';
                
+               
             })
             .catch(error => {
                 console.error("Error fetching data: ", error);
-                dataTableBody.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
+                cardContainer.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
             })
             .finally(() => {
                 // Code to close modal and clean up, if necessary
@@ -126,4 +110,206 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 
             });
     });
+
+    
+    foodButton.addEventListener('click', function() {
+        const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
+        
+        fetch(`/services`)
+        .then(response => response.json())
+        .then(data => {
+                cardContainer.innerHTML = '';// Clear existing data
+                let resultsFound = false;
+                
+                data.forEach(entry => {
+
+                        if (entry[search].toLowerCase().includes(keyword) && entry["taxonomy_name"].toLowerCase().includes("food")) {
+                            resultsFound = true;
+
+                            const card = document.createElement('div');
+                            card.classList.add('card');
+
+                            // Append elements to the card
+                            const cardBody = document.createElement('div');
+                            cardBody.classList.add('card-body');
+                            const serviceNameElement = document.createElement('h1');
+                            serviceNameElement.textContent = entry["agency_name"];
+
+                            cardBody.appendChild(serviceNameElement);
+
+
+                            const contactDiv = document.createElement('div');
+
+                            const emailElement = document.createElement('a');
+                            emailElement.classList.add('card-elem-gray');
+                            emailElement.textContent = entry["service_email"];
+                            contactDiv.appendChild(emailElement);
+
+                            const websiteElement = document.createElement('a');
+                            websiteElement.classList.add('card-elem-gray');
+                            websiteElement.textContent = entry["service_website"];
+                            contactDiv.appendChild(websiteElement);
+
+
+                            cardBody.appendChild(contactDiv);
+
+                            const descriptionElement = document.createElement('p');
+                            descriptionElement.textContent = entry["angecny_desc"];
+
+                            const hrElement = document.createElement('hr');
+
+                            cardBody.appendChild(descriptionElement);
+                            cardBody.appendChild(hrElement);
+
+                            const addressElement = document.createElement('p');
+                            addressElement.classList.add('card-elem-gray');
+                            addressElement.textContent = entry["address_1"];
+
+                            cardBody.appendChild(addressElement);
+
+                            const numberElement = document.createElement('p');
+                            numberElement.classList.add('card-elem-gray');
+                            numberElement.textContent = entry["site_number"];
+
+                            cardBody.appendChild(numberElement);
+
+                            const scheduleElement = document.createElement('p');
+                            scheduleElement.classList.add('card-elem-gray');
+                            scheduleElement.textContent = entry["site_schedule"];
+
+                            cardBody.appendChild(scheduleElement);
+                            card.appendChild(cardBody);
+
+                            // Append the card to the container
+                            cardContainer.appendChild(card);
+
+                        
+                    }
+                }
+                
+                );
+
+                if (!resultsFound) {
+                    cardContainer.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
+                }
+
+                $('#dataIndexModal').modal('hide');
+                
+               
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                cardContainer.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
+            })
+            .finally(() => {
+                // Code to close modal and clean up, if necessary
+                $("#dataIndexModal").modal('hide');
+                $("body").removeClass('modal-open');
+                $('.modal-backdrop').remove();
+                
+            });
+
+        });
+
+
+        housingButton.addEventListener('click', function() {
+            const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
+            
+            fetch(`/services`)
+            .then(response => response.json())
+            .then(data => {
+                    cardContainer.innerHTML = '';// Clear existing data
+                    let resultsFound = false;
+                    
+                    data.forEach(entry => {
+    
+                            if (entry[search].toLowerCase().includes(keyword) && entry["taxonomy_name"].toLowerCase().includes("housing")) {
+                                resultsFound = true;
+    
+                                const card = document.createElement('div');
+                                card.classList.add('card');
+    
+                                // Append elements to the card
+                                const cardBody = document.createElement('div');
+                                cardBody.classList.add('card-body');
+                                const serviceNameElement = document.createElement('h1');
+                                serviceNameElement.textContent = entry["agency_name"];
+    
+                                cardBody.appendChild(serviceNameElement);
+    
+    
+                                const contactDiv = document.createElement('div');
+    
+                                const emailElement = document.createElement('a');
+                                emailElement.classList.add('card-elem-gray');
+                                emailElement.textContent = entry["service_email"];
+                                contactDiv.appendChild(emailElement);
+    
+                                const websiteElement = document.createElement('a');
+                                websiteElement.classList.add('card-elem-gray');
+                                websiteElement.textContent = entry["service_website"];
+                                contactDiv.appendChild(websiteElement);
+    
+    
+                                cardBody.appendChild(contactDiv);
+    
+                                const descriptionElement = document.createElement('p');
+                                descriptionElement.textContent = entry["angecny_desc"];
+    
+                                const hrElement = document.createElement('hr');
+    
+                                cardBody.appendChild(descriptionElement);
+                                cardBody.appendChild(hrElement);
+    
+                                const addressElement = document.createElement('p');
+                                addressElement.classList.add('card-elem-gray');
+                                addressElement.textContent = entry["address_1"];
+    
+                                cardBody.appendChild(addressElement);
+    
+                                const numberElement = document.createElement('p');
+                                numberElement.classList.add('card-elem-gray');
+                                numberElement.textContent = entry["site_number"];
+    
+                                cardBody.appendChild(numberElement);
+    
+                                const scheduleElement = document.createElement('p');
+                                scheduleElement.classList.add('card-elem-gray');
+                                scheduleElement.textContent = entry["site_schedule"];
+    
+                                cardBody.appendChild(scheduleElement);
+                                card.appendChild(cardBody);
+    
+                                // Append the card to the container
+                                cardContainer.appendChild(card);
+    
+                            
+                        }
+                    }
+                    
+                    );
+    
+                    if (!resultsFound) {
+                        cardContainer.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
+                    }
+    
+                    $('#dataIndexModal').modal('hide');
+                    
+                   
+                })
+                .catch(error => {
+                    console.error("Error fetching data: ", error);
+                    cardContainer.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
+                })
+                .finally(() => {
+                    // Code to close modal and clean up, if necessary
+                    $("#dataIndexModal").modal('hide');
+                    $("body").removeClass('modal-open');
+                    $('.modal-backdrop').remove();
+                    
+                });
+    
+            });
+
+    
 });
