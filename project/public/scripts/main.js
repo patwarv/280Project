14443847e1ad792f49e791cdb5b2c61bd422d4fe
rhit-chungea <chungea@rose-hulman.edu>
2023-11-06 14:53:07 -0500

    can now do transportation and clothing filters

--- project/public/scripts/main.js
+++ project/public/scripts/main.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const cardContainer = document.getElementById('cardContainer');
     const foodButton = document.getElementById('foodButton');
     const housingButton = document.getElementById('housingButton');
+    const transportationButton = document.getElementById('transportationButton');
+    const clothingButton = document.getElementById('clothingButton');
 
     fetchDataButton.addEventListener('click', function() {
         const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
@@ -51,7 +53,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                             cardBody.appendChild(contactDiv);
 
                             const descriptionElement = document.createElement('p');
-                            descriptionElement.textContent = entry["angecny_desc"];
+                            descriptionElement.textContent = entry["agency_desc"];
 
                             const hrElement = document.createElement('hr');
 
@@ -154,7 +156,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                             cardBody.appendChild(contactDiv);
 
                             const descriptionElement = document.createElement('p');
-                            descriptionElement.textContent = entry["angecny_desc"];
+                            descriptionElement.textContent = entry["agency_desc"];
 
                             const hrElement = document.createElement('hr');
 
@@ -254,7 +256,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                                 cardBody.appendChild(contactDiv);
     
                                 const descriptionElement = document.createElement('p');
-                                descriptionElement.textContent = entry["angecny_desc"];
+                                descriptionElement.textContent = entry["agency_desc"];
     
                                 const hrElement = document.createElement('hr');
     
@@ -311,5 +313,202 @@ document.addEventListener('DOMContentLoaded', (event) => {
     
             });
 
-    
+
+            transportationButton.addEventListener('click', function() {
+                const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
+                
+                fetch(`/services`)
+                .then(response => response.json())
+                .then(data => {
+                        cardContainer.innerHTML = '';// Clear existing data
+                        let resultsFound = false;
+                        
+                        data.forEach(entry => {
+        
+                                if (entry[search].toLowerCase().includes(keyword) && entry["taxonomy_name"].toLowerCase().includes("transportation")) {
+                                    resultsFound = true;
+        
+                                    const card = document.createElement('div');
+                                    card.classList.add('card');
+        
+                                    // Append elements to the card
+                                    const cardBody = document.createElement('div');
+                                    cardBody.classList.add('card-body');
+                                    const serviceNameElement = document.createElement('h1');
+                                    serviceNameElement.textContent = entry["agency_name"];
+        
+                                    cardBody.appendChild(serviceNameElement);
+        
+        
+                                    const contactDiv = document.createElement('div');
+        
+                                    const emailElement = document.createElement('a');
+                                    emailElement.classList.add('card-elem-gray');
+                                    emailElement.textContent = entry["service_email"];
+                                    contactDiv.appendChild(emailElement);
+        
+                                    const websiteElement = document.createElement('a');
+                                    websiteElement.classList.add('card-elem-gray');
+                                    websiteElement.textContent = entry["service_website"];
+                                    contactDiv.appendChild(websiteElement);
+        
+        
+                                    cardBody.appendChild(contactDiv);
+        
+                                    const descriptionElement = document.createElement('p');
+                                    descriptionElement.textContent = entry["agency_desc"];
+        
+                                    const hrElement = document.createElement('hr');
+        
+                                    cardBody.appendChild(descriptionElement);
+                                    cardBody.appendChild(hrElement);
+        
+                                    const addressElement = document.createElement('p');
+                                    addressElement.classList.add('card-elem-gray');
+                                    addressElement.textContent = entry["address_1"];
+        
+                                    cardBody.appendChild(addressElement);
+        
+                                    const numberElement = document.createElement('p');
+                                    numberElement.classList.add('card-elem-gray');
+                                    numberElement.textContent = entry["site_number"];
+        
+                                    cardBody.appendChild(numberElement);
+        
+                                    const scheduleElement = document.createElement('p');
+                                    scheduleElement.classList.add('card-elem-gray');
+                                    scheduleElement.textContent = entry["site_schedule"];
+        
+                                    cardBody.appendChild(scheduleElement);
+                                    card.appendChild(cardBody);
+        
+                                    // Append the card to the container
+                                    cardContainer.appendChild(card);
+        
+                                
+                            }
+                        }
+                        
+                        );
+        
+                        if (!resultsFound) {
+                            cardContainer.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
+                        }
+        
+                        $('#dataIndexModal').modal('hide');
+                        
+                       
+                    })
+                    .catch(error => {
+                        console.error("Error fetching data: ", error);
+                        cardContainer.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
+                    })
+                    .finally(() => {
+                        // Code to close modal and clean up, if necessary
+                        $("#dataIndexModal").modal('hide');
+                        $("body").removeClass('modal-open');
+                        $('.modal-backdrop').remove();
+                        
+                    });
+        
+                });
+
+                clothingButton.addEventListener('click', function() {
+                    const keyword = keywordInput.value.toLowerCase(); // convert to lowercase for case-insensitive search
+                    
+                    fetch(`/services`)
+                    .then(response => response.json())
+                    .then(data => {
+                            cardContainer.innerHTML = '';// Clear existing data
+                            let resultsFound = false;
+                            
+                            data.forEach(entry => {
+            
+                                    if (entry[search].toLowerCase().includes(keyword) && entry["taxonomy_name"].toLowerCase().includes("clothing")) {
+                                        resultsFound = true;
+            
+                                        const card = document.createElement('div');
+                                        card.classList.add('card');
+            
+                                        // Append elements to the card
+                                        const cardBody = document.createElement('div');
+                                        cardBody.classList.add('card-body');
+                                        const serviceNameElement = document.createElement('h1');
+                                        serviceNameElement.textContent = entry["agency_name"];
+            
+                                        cardBody.appendChild(serviceNameElement);
+            
+            
+                                        const contactDiv = document.createElement('div');
+            
+                                        const emailElement = document.createElement('a');
+                                        emailElement.classList.add('card-elem-gray');
+                                        emailElement.textContent = entry["service_email"];
+                                        contactDiv.appendChild(emailElement);
+            
+                                        const websiteElement = document.createElement('a');
+                                        websiteElement.classList.add('card-elem-gray');
+                                        websiteElement.textContent = entry["service_website"];
+                                        contactDiv.appendChild(websiteElement);
+            
+            
+                                        cardBody.appendChild(contactDiv);
+            
+                                        const descriptionElement = document.createElement('p');
+                                        descriptionElement.textContent = entry["agency_desc"];
+            
+                                        const hrElement = document.createElement('hr');
+            
+                                        cardBody.appendChild(descriptionElement);
+                                        cardBody.appendChild(hrElement);
+            
+                                        const addressElement = document.createElement('p');
+                                        addressElement.classList.add('card-elem-gray');
+                                        addressElement.textContent = entry["address_1"];
+            
+                                        cardBody.appendChild(addressElement);
+            
+                                        const numberElement = document.createElement('p');
+                                        numberElement.classList.add('card-elem-gray');
+                                        numberElement.textContent = entry["site_number"];
+            
+                                        cardBody.appendChild(numberElement);
+            
+                                        const scheduleElement = document.createElement('p');
+                                        scheduleElement.classList.add('card-elem-gray');
+                                        scheduleElement.textContent = entry["site_schedule"];
+            
+                                        cardBody.appendChild(scheduleElement);
+                                        card.appendChild(cardBody);
+            
+                                        // Append the card to the container
+                                        cardContainer.appendChild(card);
+            
+                                    
+                                }
+                            }
+                            
+                            );
+            
+                            if (!resultsFound) {
+                                cardContainer.innerHTML = `<tr><td colspan="${Object.keys(data[0]).length}">No results found for "${keyword}".</td></tr>`;
+                            }
+            
+                            $('#dataIndexModal').modal('hide');
+                            
+                           
+                        })
+                        .catch(error => {
+                            console.error("Error fetching data: ", error);
+                            cardContainer.innerHTML = `<tr><td colspan="2">Error fetching data. Please try again.</td></tr>`;
+                        })
+                        .finally(() => {
+                            // Code to close modal and clean up, if necessary
+                            $("#dataIndexModal").modal('hide');
+                            $("body").removeClass('modal-open');
+                            $('.modal-backdrop').remove();
+                            
+                        });
+            
+                    });
 });
